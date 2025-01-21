7d482dace581c5dc271aab37282af8f34d566156
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2025-01-21 15:36:30 +0100

    Explorando Stateless vs Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -10,61 +10,136 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red, width: 100, height: 100,),
-                Container(color: Colors.blue, width: 50, height: 50,)
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.blue, width: 1000, height: 100,),
-                Container(color: Colors.red, width: 50, height: 50,)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(color: Colors.cyan, height: 50, width: 50,),
-                Container(color: Colors.pinkAccent, height: 50, width: 50,),
-                Container(color: Colors.purple, height: 50, width: 50,)
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: Text(
-                'Diamante Amarelo',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ElevatedButton(
-                onPressed: (){ print('Você apertou o Botão'); },
-                child: Text('Aperte o botão!')
-            )
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      )
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            shadowColor: Colors.black,
+            leading: Container(),
+            title: const Text(
+              'Tarefas',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          body: ListView(
+            children: [
+              Task('Aprender Flutter'),
+              Task('Andar de Bike'),
+              Task('Meditar'),
+              Task('Meditar'),
+              Task('Meditar'),
+              Task('Meditar'),
+              Task('Meditar'),
+              Task('Meditar'),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {}),
+        ));
   }
 }
 
+class Task extends StatefulWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
+              ),
+              Column(
+                children: [
+                  Container(
+                    color: Colors.white,
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.black26,
+                          width: 72,
+                          height: 100,
+                        ),
+                        SizedBox(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis),
+                            )),
+                        SizedBox(
+                          height: 65,
+                          width: 65,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    WidgetStateProperty.all(Colors.blue),
+                                shape: WidgetStateProperty.all(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(5)))),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up, color: Colors.white),
+                                Text(
+                                  'UP',
+                                  style: TextStyle(fontSize: 12, color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                              width: 200,
+                              child: LinearProgressIndicator(
+                                color: Colors.white,
+                                value: nivel / 10,
+                              ))),
+                      Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Nivel: $nivel',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ))
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}

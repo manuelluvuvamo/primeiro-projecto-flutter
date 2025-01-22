e1088c3b55c150c4fdf29acd9f79fff21965d78b
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2025-01-22 14:59:09 +0100

    Widget de imagens

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,16 @@ class MyApp extends StatelessWidget {
           ),
           body: ListView(
             children: [
-              Task('Aprender Flutter'),
-              Task('Andar de Bike'),
-              Task('Meditar'),
-              Task('Meditar'),
-              Task('Meditar'),
-              Task('Meditar'),
-              Task('Meditar'),
-              Task('Meditar'),
+              Task('Aprender Flutter',
+                  'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              Task('Andar de Bike',
+                  'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              Task('Meditar',
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+              Task('Ler',
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+              Task('Jogar',
+                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'),
             ],
           ),
           floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -44,7 +46,8 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome;
-  const Task(this.nome, {super.key});
+  final String foto;
+  const Task(this.nome, this.foto, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -76,6 +79,9 @@ class _TaskState extends State<Task> {
                           color: Colors.black26,
                           width: 72,
                           height: 100,
+                          child: Image.network(
+                            widget.foto,
+                          ),
                         ),
                         SizedBox(
                             width: 200,
@@ -108,7 +114,8 @@ class _TaskState extends State<Task> {
                                 Icon(Icons.arrow_drop_up, color: Colors.white),
                                 Text(
                                   'UP',
-                                  style: TextStyle(fontSize: 12, color: Colors.white),
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.white),
                                 )
                               ],
                             ),

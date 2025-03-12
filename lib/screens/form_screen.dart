b140395e5f371d2964a91e28e6f4e0fc9a99d0a6
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2025-03-12 15:41:15 +0100

    Navigatio push e pop entre a InitialScreen e a FormScreen

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -11,97 +11,128 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imageController = TextEditingController();
-
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          shadowColor: Colors.black,
-          leading: Container(),
-          title: const Text(
-            'Nova Tarefa',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: Center(
-            child: SingleChildScrollView(
-          child: Container(
-            height: 650,
-            width: 375,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.black12,
-                border: Border.all(width: 3)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                      controller: nameController,
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Nome',
-                          fillColor: Colors.white70,
-                          filled: true)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      controller: difficultyController,
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Dificuldade',
-                          fillColor: Colors.white70,
-                          filled: true)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                      onChanged: (text) {
-                        setState(() {});
-                      },
-                      keyboardType: TextInputType.url,
-                      controller: imageController,
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Imagem',
-                          fillColor: Colors.white70,
-                          filled: true)),
-                ),
-                Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(width: 2, color: Colors.blue)),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(imageController.text, errorBuilder:
-                            (BuildContext context, Object exception,
-                                StackTrace? stackTrace) {
-                          return Image.asset(
-                            'assets/images/nophoto.webp',
-                            fit: BoxFit.cover,
-                          );
-                        }, fit: BoxFit.cover))),
-                ElevatedButton(
-                    onPressed: () {
-                      print(nameController.text);
-                      print(int.parse(difficultyController.text));
-                      print(imageController.text);
-                    },
-                    child: Text('Adicionar'))
-              ],
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            shadowColor: Colors.black,
+              iconTheme: IconThemeData(
+                color: Colors.white, //change your color here
+              ),
+            title: const Text(
+              'Nova Tarefa',
+              style: TextStyle(color: Colors.white),
             ),
           ),
-        )));
+          body: Center(
+              child: SingleChildScrollView(
+            child: Container(
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black12,
+                  border: Border.all(width: 3)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                        validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Por favor insira um nome da tarefa';
+                              } else {
+                                return null;
+                              }
+                            },	
+                        controller: nameController,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Nome',
+                            fillColor: Colors.white70,
+                            filled: true)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                        validator: (value) {
+                              if (value!.isEmpty ||int.parse(value) > 5 || int.parse(value) < 1) {
+                                return 'Por favor insira a dificuldade entre 1 e 5';
+                              } else {
+                                return null;
+                              }
+                            },
+                        keyboardType: TextInputType.number,
+                        controller: difficultyController,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Dificuldade',
+                            fillColor: Colors.white70,
+                            filled: true)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                        onChanged: (text) {
+                          setState(() {});
+                        },
+                        validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Por favor insira um URL da Imagem!';
+                              } else {
+                                return null;
+                              }
+                            },
+                        keyboardType: TextInputType.url,
+                        controller: imageController,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Imagem',
+                            fillColor: Colors.white70,
+                            filled: true)),
+                  ),
+                  Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 2, color: Colors.blue)),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(imageController.text, errorBuilder:
+                              (BuildContext context, Object exception,
+                                  StackTrace? stackTrace) {
+                            return Image.asset(
+                              'assets/images/nophoto.webp',
+                              fit: BoxFit.cover,
+                            );
+                          }, fit: BoxFit.cover))),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print(nameController.text);
+                          print(int.parse(difficultyController.text));
+                          print(imageController.text);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Tarefa Adicionada')));
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text('Adicionar'))
+                ],
+              ),
+            ),
+          ))),
+    );
   }
 }
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:primeiro_projecto_flutter/components/task.dart';
+import 'package:primeiro_projecto_flutter/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -9,45 +10,40 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-  bool opacidade = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         shadowColor: Colors.black,
-        leading: Container(),
+        //leading: Container(),
         title: const Text(
           'Tarefas',
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: Duration(milliseconds: 1000),
-        child: ListView(
-          children: [
-            Task('Aprender Flutter', 'assets/images/1.png', 3),
-            Task('Andar de Bike', 'assets/images/2.webp', 2),
-            Task('Meditar', 'assets/images/3.jpeg', 5),
-            Task('Ler', 'assets/images/4.jpg', 4),
-            Task('Jogar', 'assets/images/5.avif', 1),
-            SizedBox(
-              height: 80,
-            )
-          ],
-        ),
+      body: ListView(
+        children: [
+          Task('Aprender Flutter', 'assets/images/1.png', 3),
+          Task('Andar de Bike', 'assets/images/2.webp', 2),
+          Task('Meditar', 'assets/images/3.jpeg', 5),
+          Task('Ler', 'assets/images/4.jpg', 4),
+          Task('Jogar', 'assets/images/5.avif', 1),
+          SizedBox(
+            height: 80,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            opacidade = !opacidade;
-          });
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormScreen();
+          }));
         },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(50),
         ),
-        child: Icon(Icons.remove_red_eye),
+        child: Icon(Icons.add),
       ),
     );
   }
